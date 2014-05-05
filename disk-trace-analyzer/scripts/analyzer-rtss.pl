0d1d39af5baa7c0bee8b124dbe428d2069499ef3
yixinluo <camellyx@gmail.com>
2014-05-04 20:52:50 -0400

    analyzer

--- disk-trace-analyzer/scripts/analyzer-rtss.pl
+++ disk-trace-analyzer/scripts/analyzer-rtss.pl
@@ -3,6 +3,22 @@
 use strict;
 use warnings;
 
+# Current state of the trace
+my $cur_hour = 0;
+
+# Tracking write intervals between within a flash block
+my $BLOCK_SIZE = 256*1024; # block size = 256 KB
+my $cur_page = 0; # currently written bytes within a block
+my $block_start = 0; # start hour
+
+
+# Tracking retention time
+my $PAGE_SIZE = 512;
+my @last_write;
+
+# Statistics
+my @write_intervals = ();
+
 my $dir = '../ds-bzip2/';
 
 opendir(DIR, $dir) or die $!;
@@ -16,10 +32,10 @@ my @dates
 
 closedir(DIR);
 
-my @sortedDates = sort(@dates);
+my @sorted_dates = sort(@dates);
 
 # Loop through the array printing out the filenames
-foreach my $folder (@sortedDates) {
+foreach my $folder (@sorted_dates) {
   #print "$folder\n";
   $folder = $dir.$folder."/";
 
@@ -27,9 +43,9 @@ foreach my $folder (@sortedDates) {
   my @hours = grep {/-bz2.ds.out$/} readdir(DIR);
   closedir(DIR);
 
-  my @sortedHours = sort(@hours);
+  my @sorted_hours = sort(@hours);
 
-  foreach my $file (@sortedHours) {
+  foreach my $file (@sorted_hours) {
     #print "$file\n";
     $file = $folder.$file;
     #print "$file\n";
@@ -39,11 +55,52 @@ foreach my $folder (@sortedDates) {
       my $line = $_;
       print "$line\n";
       my @fields = split(/\ /);
+        my $type = $fields[-1];
+        my $size = $fields[-2];
+        my $block = $fields[-3];
+      if ($type eq "T") {
+        # Is Write
+        if ($cur_page == 0) {
+          $block_start = $cur_hour;
+        }
+        $cur_page += $size;
+        if ($cur_page >= $BLOCK_SIZE) {
+          # Wrap around
+          push(@write_intervals, $cur_hour - $block_start + 1);
+          $cur_page %= $BLOCK_SIZE;
+          if ($cur_page > 0) {
+            $block_start = $cur_hour;
+          }
+        }
+        if ($size % $PAGE_SIZE != 0) {
+          # assertion
+          print "$line: has size $size\n";
+          exit -1;
+        }
+        while ($size > 0) {
+          $last_write[$block] = $cur_hour;
+          $size -= $PAGE_SIZE;
+          $block += $PAGE_SIZE;
+        }
+      } elsif ($type eq "F") {
+        # Is Read
+        while ($size > 0) {
+          my $retention = $cur_hour - $last_write[$block];
+          print $retention;
+          $size -= $PAGE_SIZE;
+          $block += $PAGE_SIZE;
+        }
+      }
+    } # end FILE
+    close(FILE);
+    $cur_hour++;
+    last;
+  } # end files
+  last;
+} # end folders
 
-      #debug
-      exit 0;
-    }
-  }
-}
+# Print statistics
+local $" = "\n";
+#print "write interval: \n@write_intervals\n";
 
 exit 0;
