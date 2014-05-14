7c709e545cdd546b967b19d71ab9451806662303
Yixin Luo <yixinluo@gs12632.sp.cs.cmu.edu>
2014-05-14 14:18:21 -0400

    parse-hash

--- disk-trace-analyzer/scripts/parse-hash.pl
+++ disk-trace-analyzer/scripts/parse-hash.pl
@@ -3,23 +3,39 @@
 use strict;
 use warnings;
 
-open(FILE, $ARGV[0]);
+open(FILE, $ARGV[0]) or die $!;
 
-my $start = 0;
-while (<FILE>) {
-  chomp;
-  my $line = $_;
-  if ($start == 0) {
+if ($ARGV[1] eq "wri") {
+  # extract write intervals
+  while (<FILE>) {
+    chomp;
+    my $line = $_;
     if ($line =~ /retention\ times/) {
-      $start = 1;
+      last;
     }
-    next;
+    my @fields = split(/['\ =>\ ,]+/, $line);
+    if ($#fields >= 2) {
+      print "$fields[1], $fields[2]\n";
+    }
   }
-  #print "$line\n";
-  my @fields = split(/['\ =>\ ,]+/, $line);
-  if ($#fields >= 2) {
-    print "$fields[1],$fields[2]\n";
+} else {
+  # extract retention times
+  my $start = 0;
+  while (<FILE>) {
+    chomp;
+    my $line = $_;
+    if ($start == 0) {
+      if ($line =~ /retention\ times/) {
+        $start = 1;
+      }
+      next;
+    }
+    my @fields = split(/['\ =>\ ,]+/, $line);
+    if ($#fields >= 2) {
+      print "$fields[1], $fields[2]\n";
+    }
   }
 }
+
 
 exit 0;
