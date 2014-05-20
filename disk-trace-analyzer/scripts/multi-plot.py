1ae5c1e946d6a1d443c081c10536d71ec543edb2
Yixin Luo <camellyx@gmail.com>
2014-05-20 00:43:06 -0400

    multiplot 1 month

--- disk-trace-analyzer/scripts/multi-plot.py
+++ disk-trace-analyzer/scripts/multi-plot.py
@@ -7,11 +7,24 @@ import csv as csv
 import sys as sys
 import os as os
 import glob as glob
+from matplotlib.ticker import ScalarFormatter
+from matplotlib.ticker import FuncFormatter
+
+def to_percent(y, position=0):
+    # Ignore the passed in position. This has the effect of scaling the default
+    # tick locations.
+    s = str(100 * y)
+
+    # The percent symbol needs escaping in latex
+    if matplotlib.rcParams['text.usetex'] == True:
+        return s + r'$\%$'
+    else:
+        return s + '%'
 
 sns.set_style({"font.family": "sans-serif"})
 
 # find all files
-f, axes = plt.subplots(3, 6, figsize = (16,8))
+f, axes = plt.subplots(4, 5, figsize = (16,8), sharex=True, sharey=True)
 plt.subplots_adjust(hspace=0.4)
 files = glob.glob("*.csv")
 fx = 0
@@ -22,6 +35,7 @@ for filename in files:
     y0 = [0]*100
     y0_day = []
     maxx = 0
+    sumx = 0
     csvfile = open(filename, 'rb')
     next(csvfile)
     rows = csv.reader(csvfile)
@@ -32,6 +46,10 @@ for filename in files:
         y0_day.append(0)
         for j in range(0,23):
             y0_day[i] += y0[i*24+j]
+            sumx += y0[i*24+j]
+    for i in range(0,4):
+        if (sumx > 0):
+            y0_day[i] = (1.0*y0_day[i])/sumx * 100
     #y0 = y0[:maxx]
     #y0 = np.asarray(y0[:maxx])
     #for i in range(1,maxx+1):
@@ -42,17 +60,19 @@ for filename in files:
     print x0, y0_day
     #axes[fx,fy].plot(x0,y0_day,label=filename) # line plot
     sns.barplot(x0,y0_day, ci=None, hline = 0, ax=axes[fy,fx])
-    axes[fy,fx].set_xlabel(filename[9:-4])
-    axes[fy,fx].ticklabel_format(axis='y', style='sci',
-            scilimits=(2,2))
+    #plt.gca().yaxis.set_major_formatter(FuncFormatter(to_percent))
+    axes[fy,fx].set_title(filename[9:-4])
+    #axes[fy,fx].ticklabel_format(axis='y', style='sci', scilimits=(2,2))
+    if (fx == 0):
+        axes[fy,fx].set_ylabel("percentage")
+    if (fy == 3):
+        axes[fy,fx].set_xlabel("days")
     if (maxx == 0):
-        axes[fy,fx].set_ylim(0,5)
+        axes[fy,fx].set_ylim(0,100)
     fx += 1
-    if (fx == 6):
+    if (fx == 5):
         fx = 0
         fy += 1
-
-axes[2,5].axis('off')
 
 #plt.yscale('log')
 #plt.legend()
